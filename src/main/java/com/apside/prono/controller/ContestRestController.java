9755fe16e562c3f9c45598d8757347845e00188b
claire41 <lebertclaire@yahoo.fr>
2019-07-24 14:41:59 +0200

    modif cross origin

--- src/main/java/com/apside/prono/controller/ContestRestController.java
+++ src/main/java/com/apside/prono/controller/ContestRestController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.apside.prono.model.Contest;
 import com.apside.prono.service.ContestService;
 
+@CrossOrigin(origins = "http//localhost:4200")
 @RestController
 public class ContestRestController {
 
--- src/main/java/com/apside/prono/controller/HomeController.java
+++ src/main/java/com/apside/prono/controller/HomeController.java
@@ -2,10 +2,12 @@ package com.apside.prono.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http//localhost:4200")
 @RestController
 @RequestMapping(value = "/")
 public class HomeController {
